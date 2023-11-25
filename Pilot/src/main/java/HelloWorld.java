8c47e4e8ddd17c0cbad9eb24a3d1564c026846ef
ilyassolayman <illzsolayman@gmail.com>
2023-11-25 16:06:44 +0000

    Change

--- Pilot/src/main/java/HelloWorld.java
+++ Pilot/src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        String x = "My name is Ilyas Solayman!!";
+        String x = "My name is Ilyas Solayman!";
         System.out.println(x);
     }
 }
