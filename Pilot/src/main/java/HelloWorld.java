bafeb604bc5f2d275f741369e88a08425d348358
Ilyas Solayman <ilyassolayman@Ilyass-MBP.cust.communityfibre.co.uk>
2023-11-25 15:54:31 +0000

    Defining gitignore

--- Pilot/src/main/java/HelloWorld.java
+++ Pilot/src/main/java/HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        String x = "Hello world 1!";
+        System.out.println(x);
     }
 }
